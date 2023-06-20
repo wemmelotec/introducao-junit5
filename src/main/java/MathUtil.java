0285895a5ee0a73c2bca7ea13d7b19674c5b1b4a
wellington magno <tonmelo@hotmail.com>
2023-06-20 13:54:22 -0300

    implementei o teste da P7 do mdc

--- src/main/java/MathUtil.java
+++ src/main/java/MathUtil.java
@@ -4,7 +4,7 @@ Sendo assim, os métodos serão estáticos para possibilitar o acesso sem precis
  */
 public class MathUtil {
     //uma vez que eu tenha uma classe com métodos eu posso criar uma classe de teste
-    public static int mdc(int a, int b){
+    public static int mdc(int a, int b) {
         //escrever o mínimo de código possível para resolver o problema do teste 1 propriedade
         //segunda versão do passed
         //if(b > 0){
@@ -12,14 +12,31 @@ public class MathUtil {
         //        return b;
         //    }
         //}
-        //terceira versão do refectory
-        if (b>0 && a%b ==0){
+
+        //esse código foi para resolver P7 e o erro inserido por essas três linhas abaixo no mdcANegativoP3
+        a = Math.abs(a);
+        b = Math.abs(b);
+        //esse código foi inserido para resolver a P6
+        //essas três linhas evitam que eu faça um if para ordenar
+        //quando eu fiz a inclusão desse código eu o quebrei e isso foi detectado no teste completo
+        //pois falhou o mdcANegativoP3
+        final int maior = Math.max(a, b);
+        b = Math.min(a, b);
+        a = maior;
+
+        //terceira versão do refactory P1
+        if (b > 0 && a % b == 0) {
             return b;
         }
 
         //esse método é a segunda versão do mdcP3
-        if(b==0){
+        if (b == 0) {
             return Math.abs(a);
+        }
+
+        //esse método foi incluído por conta da propriedade mdcP5
+        if (a % b != 0) {
+            return 1;
         }
         //primeira versão do failed
         return -1;//mdc inválido
--- src/test/java/MathUtilTest.java
+++ src/test/java/MathUtilTest.java
@@ -11,56 +11,107 @@ class MathUtilTest {
         final int a = 6;
         final int b = 3;
         final int esperado = 3;
-        final int obtido = MathUtil.mdc(a,b);
-        assertEquals(esperado,obtido);
-    }
-    @Test
-    //utilizo o final para não ficar testando com outro valores ao invés de criar novos teste apenas alterar o valor
-    void mdcAImparP1(){
-        final int a = 9;
-        final int b = 3;
-        final int esperado = 3;
-        final int obtido = MathUtil.mdc(a,b);
-        assertEquals(esperado,obtido);
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
     }
 
     @Test
-    void mdcParesP2(){
-    //Todo número que for divisor comum de a  e  b também é um divisor de m d c ( a , b ) mdc(a,b);
-    //como na primeira chamada minha função não falhou preciso ficar alerta pois eu ainda não havia escrito código pra ela
-    //mas como a função já resolver esse teste da P2 eu posso refatorar o teste nesse momento
+        //utilizo o final para não ficar testando com outro valores ao invés de criar novos teste apenas alterar o valor
+    void mdcAImparP1() {
+        final int a = 9;
+        final int b = 3;
+        final int esperado = 3;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void mdcParesP2() {
+        //Todo número que for divisor comum de a  e  b também é um divisor de m d c ( a , b ) mdc(a,b);
+        //como na primeira chamada minha função não falhou preciso ficar alerta pois eu ainda não havia escrito código pra ela
+        //mas como a função já resolver esse teste da P2 eu posso refatorar o teste nesse momento
         final int a = 16;
         final int b = 8;
         final int divisor = 4;
         //final int esperado = 0;
-        final int obtido = MathUtil.mdc(a,b);
+        final int obtido = MathUtil.mdc(a, b);
         //assertEquals(esperado, obtido % divisor);
         assertTrue(obtido % divisor == 0);
     }
 
     @Test
-    void mdcImparP2(){
+    void mdcImparP2() {
         final int a = 9;
         final int b = 3;
         final int divisor = 3;
-        final int obtido = MathUtil.mdc(a,b);
+        final int obtido = MathUtil.mdc(a, b);
         assertTrue(obtido % divisor == 0);
         //importante lembrar que eu devo pensar neste mesmo modelo de teste com outros valores
     }
 
     @Test
-    void mdcAPositivoP3(){
+    void mdcAPositivoP3() {
         final int a = 16;
         final int b = 0;
         final int esperado = 16;
-        final int obtido = MathUtil.mdc(a,b);
-        assertEquals(esperado,obtido);
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
     }
+
     @Test
-    void mdcANegativoP3(){
+    void mdcANegativoP3() {
         final int a = -16;
         final int b = 0;
         final int esperado = 16;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void mdcMParP4() {
+        final int a = 9;
+        final int b = 3;
+        final int m = 2;
+        //mdc(a*m, b*m) == m * mdc(a,b)
+        //mdc(18,6) == 2 * mdc(9,3)
+        // 6 == 2 * 3
+        final int esperado = MathUtil.mdc(a * m, b * m);
+        final int obtido = m * MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+
+    //A propriedade P5 precisa dividir o teste em duas partes
+    @Test
+    void mdcP5() {
+        //se mdc(a,b) ==1, então mdc(a*b, c) == mdc(b,c)
+        //tenho que escolher valores que deem o mdc = 1, de acordo com a minha condição
+        final int a = 7;
+        final int b = 3;
+        final int c = 5;
+        final int esperado = MathUtil.mdc(a * b, c);
+        final int obtido = MathUtil.mdc(b, c);
+        //assertEquals(esperado,obtido);
+        //o teste passou de primeira, quando o  teste passa sem eu ter escrito código para ele
+        //eu preciso desconfiar do resultado
+        assertEquals(esperado, 1);
+    }
+
+    @Test
+    void mdcP6() {
+        //mdc(a,b) == mdc(b,a)
+        final int a = 8;
+        final int b = 2;
+        final int esperado = MathUtil.mdc(a, b);
+        final int obtido = MathUtil.mdc(b, a);
+        assertEquals(esperado, obtido);
+
+    }
+
+    @Test
+    void mdcP7TudoNegativo(){
+        final int a = -12;
+        final int b = -6;
+        final int esperado = 6;
         final int obtido = MathUtil.mdc(a,b);
         assertEquals(esperado,obtido);
     }
